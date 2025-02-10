904c170f3fa94f7ef1c7aedb5ab0d647364271ed
forlwow <forlwow@outlook.com>
2025-02-10 18:55:45 +0800

    some changes

--- cmake-build-debug/CMakeFiles/web_server.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/web_server.dir/DependInfo.cmake
@@ -11,6 +11,7 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "/home/worker/webserver/server/main.cpp" "CMakeFiles/web_server.dir/server/main.cpp.o" "gcc" "CMakeFiles/web_server.dir/server/main.cpp.o.d"
   "/home/worker/webserver/server/test17.cpp" "CMakeFiles/web_server.dir/server/test17.cpp.o" "gcc" "CMakeFiles/web_server.dir/server/test17.cpp.o.d"
   "/home/worker/webserver/server/test20.cpp" "CMakeFiles/web_server.dir/server/test20.cpp.o" "gcc" "CMakeFiles/web_server.dir/server/test20.cpp.o.d"
+  "/home/worker/webserver/test/test_main.cpp" "CMakeFiles/web_server.dir/test/test_main.cpp.o" "gcc" "CMakeFiles/web_server.dir/test/test_main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
